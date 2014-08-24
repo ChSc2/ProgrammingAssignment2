2b6a9dc3bca7ed21b73fc1f0035bb48c234c6b47
ChSc2 <chr.schuettler@gmail.com>
2014-08-24 23:09:34 +0200

    Finished Assignemt

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# Function will store a passed matrix as well as the inverse after
+# it has been calculated
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # Initialize the object for the inverted matrix
+  # outside for scoping
+  InvMatrix <- NULL
+  
+  # Sets the initial value and nulls the inverted matrix
+  # in case we have something stored that we want to override
+  set <- function(y) {
+    x <<- y
+    InvMatrix <<- NULL
+  }
+  
+  # Returns the original matrix
+  get <- function() x
+  
+  #Sets and gets the inverse
+  setinverse  <- function(inverse) InvMatrix <<- inverse 
+  getinverse  <- function() InvMatrix
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+# Function will check within a passed cacheMatrix if the inverse has
+# been set in the object. Will print it out if so, otherwise calculate
+# and set it within the cacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  # Get the value from the object
+  InvMatrix <- x$getinverse ()
+
+  # Has it already been calculated and set within the cacheMatrix
+  if(!is.null(InvMatrix)) {
+    message("Getting cached matrix")
+  }
+  else
+  {
+    # Get the original object
+    data <- x$get()
+    # Calculate the inverse
+    InvMatrix <- solve(data, ...)
+    # Store inverse in cacheMatrix
+    x$setinverse(InvMatrix)    
+  }
+  
+  # Print
+  InvMatrix
 }
